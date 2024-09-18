bb06ef5a44f499ded8954a2c5d8578d4de527afd
hoisharka <hoisharka@gmail.com>
2024-09-18 20:01:22 +0900

    examples 하위 프로젝트에서 lint관련 패키지 제거. root에서 lint명령어 동작하도록 설정.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,28 @@
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-	},
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
+  overrides: [
+    {
+      files: ['**/examples/*/**/*.{ts,tsx}'], // examples 디렉토리의 ts, tsx 파일들에 대해
+      extends: [
+        'plugin:react/recommended', // React 관련 규칙 추가
+      ],
+      plugins: ['react-hooks'],
+      settings: {
+        react: {
+          version: 'detect', // React 버전 자동 감지
+        },
+      },
+    },
+  ],
 };
